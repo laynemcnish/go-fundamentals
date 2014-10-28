fb26ab70c1ab360ed448c472fb6ade9bf6142575
Peter Grunde <peter.a.grunde@gmail.com>
2014-10-28 10:46:56 -0600

    ultra-iota extra credit written and tested

--- 01-constants-variables/you-do/main.go
+++ 01-constants-variables/you-do/main.go
@@ -63,11 +63,26 @@ type fishMeasure struct {
 	Weight      float32
 }
 
-// create a series of 3 iota constants, but skip the first iota value
+// create a series of 3 iota constants, but skip the first iota value 0
 // Name them Caesar, Crassus, and Pompeius in that order (I better not see any numbers!)
 const (
 	_ int = iota
 	Caesar
 	Crassus
 	Pompeius
+)
+
+// ultra-iota: use iota to create a complex set of values for six nutrient constants.
+// !!! Never type a number larger than 2 and no more than two numbers written !!!
+// declare the constants in the following order:
+// minerals come first and should have a value of 1
+// fat is next with a value of 0
+// vitamins and protein next, both with values of 2
+// carbohydrates after that with a value of 3
+// finally comes water with a value of 6
+const (
+	minerals, fat int = iota + 1, iota * 2
+	vitamins, protein
+	carbohydrates, _
+	_, water
 )
--- 01-constants-variables/you-do/main_test.go
+++ 01-constants-variables/you-do/main_test.go
@@ -175,3 +175,24 @@ func Test_triumverate_values(t *testing.T) {
 		t.Errorf("Something went wrong with Pompeius iota: %v", Pompeius)
 	}
 }
+
+func Test_nutrient_values(t *testing.T) {
+	if minerals != 1 {
+		t.Errorf("Something went wrong with minerals iota: %v", minerals)
+	}
+	if fat != 0 {
+		t.Errorf("Something went wrong with fat iota: %v", fat)
+	}
+	if vitamins != 2 {
+		t.Errorf("Something went wrong with vitamins iota: %v", vitamins)
+	}
+	if protein != 2 {
+		t.Errorf("Something went wrong with protein iota: %v", protein)
+	}
+	if carbohydrates != 3 {
+		t.Errorf("Something went wrong with carbohydrates iota: %v", carbohydrates)
+	}
+	if water != 6 {
+		t.Errorf("Something went wrong with water iota: %v", water)
+	}
+}

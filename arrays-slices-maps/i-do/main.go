8279b9008f4da99ebac66f05a5e5d9b890c16999
Peter Grunde <peter.a.grunde@gmail.com>
2014-11-03 10:53:05 -0700

    no tests for i-do array slice map yet, examples done

--- arrays-slices-maps/i-do/main.go
+++ arrays-slices-maps/i-do/main.go
@@ -4,7 +4,6 @@ import "fmt"
 
 func main() {
 	// ARRAYS
-
 	// create an array arr that holds 7 integers
 	var arr [7]int
 
@@ -14,13 +13,10 @@ func main() {
 	// declare and initialize an array called names which contains "Peter", "Shelby", and "Zach" in that order
 	names := [3]string{"Peter", "Shelby", "Zach"}
 
-	// use fmt.Println to print the length of names
-	fmt.Println("length of names: ", len(names))
-
 	// SLICES
 
 	// slices are typed only by the elements they contain, not the number of elements
-	// declare and initialize a slice named gophers of strings, each with a length of 5.
+	// declare and initialize a slice of strings named gophers, each with a length of 5.
 	gophers := make([]string, 5)
 
 	// at index 0 of gophers set the value equal to "Mac" and 1 to "Tosh"
@@ -41,6 +37,9 @@ func main() {
 	// within cartog declare the "long" key to equal -86.238333 and the "lat" key to equal 39.203056
 	cartog["long"] = -86.238333
 	cartog["lat"] = 39.203056
+
+	// use fmt.Println to print the length of names
+	fmt.Println("length of names: ", len(names))
 
 	// print out the map cartog
 	fmt.Println("Map:", cartog)
--- arrays-slices-maps/i-do/main_test.go
+++ arrays-slices-maps/i-do/main_test.go
@@ -1,1 +1,8 @@
 package main
+
+// How to test inside the main() function???
+// func Test_arr_length(t *testing.T) {
+// 	if len(arr) != 7 {
+// 		t.Errorf("arr length equals %v, expected %v", len(arr), 7)
+// 	}
+// }

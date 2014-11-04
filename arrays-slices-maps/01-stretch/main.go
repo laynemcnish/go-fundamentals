cf13144f1ad83b22f4a855972eaf10fb549a962e
Peter Grunde <peter.a.grunde@gmail.com>
2014-11-04 09:46:13 -0700

    stretch 01 in arrays-slices-maps done without testing

--- arrays-slices-maps/01-stretch/main.go
+++ arrays-slices-maps/01-stretch/main.go
@@ -1,3 +1,18 @@
 package main
 
-// create an array whose contents are filled with iota values
+import "fmt"
+
+// create a map grades with the keys "danger", "normal", "good" pointing to slices of integers.
+// "danger" array should be filled with integers 0-6, "good" should be 7, 8, and 9, and
+// "good" should be an array containing 10. Print each value of grades with a description
+
+func main() {
+	grades := make(map[string][]int)
+	grades["danger"] = []int{0, 1, 2, 3, 4, 5, 6}
+	grades["normal"] = []int{7, 8, 9}
+	grades["good"] = []int{10}
+
+	fmt.Println("Danger: ", grades["danger"])
+	fmt.Println("Normal: ", grades["normal"])
+	fmt.Println("Good:   ", grades["good"])
+}

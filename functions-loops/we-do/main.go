0e78aa671cf4b8f3a7088c1ac194fce6a7463e60
Peter Grunde <peter.a.grunde@gmail.com>
2014-11-05 10:23:14 -0700

    we-do range function-loops complete

--- functions-loops/we-do/main.go
+++ functions-loops/we-do/main.go
@@ -1,1 +1,18 @@
 package main
+
+import "strconv"
+
+/*
+  Resources: https://code.google.com/p/go-wiki/wiki/Range
+             http://golang.org/pkg/strconv/
+
+  TODO: create a function frontRange which takes slice of strings. It should return a single string which
+  contains a numbered list of the strings such that ["one", "two"] would yield "1. One\n2.Two\n"
+  HINT: import "strconv" and use Itoa */
+func frontRange(arr []string) string {
+  var finalStr string
+  for i, str := range arr {
+    finalStr = finalStr + strconv.Itoa(i + 1) + ". " + str + "\n"
+  }
+  return finalStr
+}
--- functions-loops/we-do/main_test.go
+++ functions-loops/we-do/main_test.go
@@ -1,1 +1,20 @@
 package main
+
+import "testing"
+
+func Test_frontRange(t *testing.T) {
+	type test struct {
+		arr      []string
+		expected string
+	}
+	tests := []test{
+		test{arr: []string{"One", "Two"}, expected: "1. One\n2. Two\n"},
+    test{arr: []string{"Thing", "Other", "Another"}, expected: "1. Thing\n2. Other\n3. Another\n"},
+	}
+  for _, tt := range tests {
+    result := frontRange(tt.arr)
+    if result != tt.expected {
+      t.Errorf("frontRange(%v) = \n%v, want \n%v", tt.arr, result, tt.expected)
+    }
+  }
+}

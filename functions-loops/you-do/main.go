160e4d33dbea509bcaa09da0d6ed443aec35251d
Peter Grunde <peter.a.grunde@gmail.com>
2014-11-05 15:09:28 -0700

    function-loop you-do foldIt complete

--- functions-loops/you-do/main.go
+++ functions-loops/you-do/main.go
@@ -1,1 +1,24 @@
 package main
+
+import "fmt"
+
+// write a function corruption which takes in an integer. Print out "4 damage taken from Corruption!" as many times
+// as the integer argument. Return the total damage taken at the end.
+func corruption(n int) int {
+	for i := 0; i < n; i++ {
+		fmt.Println("4 damage taken from Corruption!")
+	}
+	return n * 4
+}
+
+// write a function foldIt which takes a slice of integers and returns two integers. The first return
+// should be the result of adding all the integers in the slice, the second a result from multiplying.
+func foldIt(arr []int) (int, int) {
+	var a int
+	b := 1
+	for _, num := range arr {
+		a += num
+		b = b * num
+	}
+	return a, b
+}
--- functions-loops/you-do/main_test.go
+++ functions-loops/you-do/main_test.go
@@ -1,1 +1,37 @@
 package main
+
+import "testing"
+
+func Test_corruption(t *testing.T) {
+	type test struct {
+		n        int
+		expected int
+	}
+	tests := []test{
+		test{n: 4, expected: 16},
+		test{n: 5, expected: 20},
+	}
+	for _, tt := range tests {
+		result := corruption(tt.n)
+		if result != tt.expected {
+			t.Errorf("corruption(%v) = %v, want %v", tt.n, result, tt.expected)
+		}
+	}
+}
+
+func Test_foldIt(t *testing.T) {
+	type test struct {
+		arr       []int
+		expected1 int
+		expected2 int
+	}
+	tests := []test{
+		test{arr: []int{1, 2, 3, 4}, expected1: 10, expected2: 24},
+	}
+	for _, tt := range tests {
+		result1, result2 := foldIt(tt.arr)
+		if result1 != tt.expected1 || result2 != tt.expected2 {
+			t.Errorf("foldIt(%v) = %v, %v; wanted %v, %v", tt.arr, result1, result2, tt.expected1, tt.expected2)
+		}
+	}
+}

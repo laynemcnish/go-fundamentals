e1bb215df93476983becdb30a0f31d6b0051c68c
Kevin Gillette <extemporalgenome@gmail.com>
2014-11-07 14:18:21 -0700

    Made blank constant untyped.

--- 01-constants-variables/03-stretch/main.go
+++ 01-constants-variables/03-stretch/main.go
@@ -3,7 +3,7 @@ package main
 // create a series of 3 iota constants, but skip the first iota value 0
 // Name them Caesar, Crassus, and Pompeius in that order (I better not see any numbers!)
 const (
-	_ int = iota
+	_ = iota
 	Caesar
 	Crassus
 	Pompeius

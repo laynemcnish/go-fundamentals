106d6522a450826c04bda9dbacd5393d9ee1b337
Peter Grunde <peter.a.grunde@gmail.com>
2014-11-09 22:28:35 -0700

    function-loop stretch 01 first problem comment added

--- functions-loops/01-stretch/main.go
+++ functions-loops/01-stretch/main.go
@@ -1,1 +1,11 @@
 package main
+
+/*
+  Resources: http://www.golang-book.com/8/index.htm
+             http://www.golang-book.com/9/index.htm
+
+  TODO: write a function Move which takes in a type position struct for an argument.
+  Assume you are passing a struct which contains integers x, y, dx, and dy. Have the function
+  return two values which represent x + dx and y + dy.
+
+*/

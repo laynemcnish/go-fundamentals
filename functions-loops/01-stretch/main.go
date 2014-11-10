6882e81f2a5cc98bf7ad98b802ceac28f0e6f25f
Peter Grunde <peter.a.grunde@gmail.com>
2014-11-09 23:29:44 -0700

    function loop 01 stretch 2nd question added

--- functions-loops/01-stretch/main.go
+++ functions-loops/01-stretch/main.go
@@ -8,4 +8,8 @@ package main
   Assume you are passing a struct which contains integers x, y, dx, and dy. Have the function
   return two values which represent x + dx and y + dy.
 
+  TODO: write a METHOD called RateChange whose receiver is a pointer to a Position struct. The method
+  should take in two integer arguments, the first which mutates dx on the Position struct by addition. The
+  second integer mutates dy on the Position struct by addition. Return the struct
+
 */
